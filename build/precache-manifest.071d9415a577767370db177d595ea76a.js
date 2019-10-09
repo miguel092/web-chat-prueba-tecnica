7d59f18bf0aeb7929a6b98959bd15eb80039b434
MIGUEL <miguelmejia92@outlook.com>
2019-10-09 11:29:04 -0500

    Hacer responsivo el boton Enviar

--- build/precache-manifest.071d9415a577767370db177d595ea76a.js
+++ build/precache-manifest.071d9415a577767370db177d595ea76a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "155ca38d88fa47b3c164badf430683ad",
+    "revision": "567624e877c382247aa70a5acefd97f8",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "1328a2b8e02206215c10",
+    "revision": "c023fef3f5e1fa59e89d",
     "url": "/static/css/main.a7c70680.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6bd6d063.chunk.js"
   },
   {
-    "revision": "1328a2b8e02206215c10",
-    "url": "/static/js/main.70762c1e.chunk.js"
+    "revision": "c023fef3f5e1fa59e89d",
+    "url": "/static/js/main.75b12165.chunk.js"
   },
   {
     "revision": "3d40a493058d59f41dfc",
